88708679e42df2e576246e00cffa4d377e82cb73
Oliver Haffenden <oliver.haffenden@bbc.co.uk>
2024-02-13 13:43:44 +0000

    Update for new server

--- theseus-xml-canary/update-canary.bat
+++ theseus-xml-canary/update-canary.bat
@@ -1,3 +1,3 @@
 tar -a -c -f theseus-xml-canary.zip nodejs
 aws s3 cp theseus-xml-canary.zip s3://theseus-misc-bucket
-aws synthetics update-canary --name theseus-xml-canary --region eu-west-2 --code S3Bucket=theseus-misc-bucket,S3Key=theseus-xml-canary.zip,Handler=theseus-xml-canary.handler --run-config EnvironmentVariables={EXPECTED_RX_LIST="ENCble\,ENCwof\,THES04\,THES05\,THES06\,THES07\,THES08\,THES12\,THES15"}
+aws synthetics update-canary --name theseus-xml-canary --region eu-west-2 --code S3Bucket=theseus-misc-bucket,S3Key=theseus-xml-canary.zip,Handler=theseus-xml-canary.handler --run-config EnvironmentVariables={EXPECTED_RX_LIST="ENCble\,ENCwof\,THES04\,THES05\,THES06\,THES07\,THES12\,THES15"}
