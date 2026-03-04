962bdb2882765959b618af3deb7ea7e959524e3a
Oliver Haffenden <oliver.haffenden@bbc.co.uk>
2026-03-04 14:26:40 +0000

    Updated for latest API version Added check for data updated recently (js version of Theseus doesn't expunge receivers)

--- theseus-xml-canary/nodejs/test-canary.js
+++ theseus-xml-canary/nodejs/test-canary.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 
 console.log('In test-canary.js')
 
-const expectedRxList = 'ENCble,ENCwof,THES04,THES05,THES06,THES07,THES08,THES12,THES15';
-
-var strXML = fs.readFile('./theseus_output.xml', async function(err,data) {
+//const expectedRxList = 'TMS123,THES15,ENCble,THES04,ENCwof,THES05,THES07,THES08,THES06,THES12';
+const expectedRxList = 'TMS123,TMS002,TMS003,TMS008,TMS014,TMS016,TMS019,THES05,THES06,THES07,TMS789'
+//var strXML = fs.readFile('./theseus_output.xml', async function(err,data) {
+var strXML = fs.readFile('./receivers.xml', async function(err,data) {
     if (err) throw(err);
     const result = await theseus_xml_checker.checkXML(data, expectedRxList);
     console.log(result);
--- theseus-xml-canary/update-canary.bat
+++ theseus-xml-canary/update-canary.bat
@@ -1,3 +1,3 @@
 tar -a -c -f theseus-xml-canary.zip nodejs
-aws s3 cp theseus-xml-canary.zip s3://theseus-misc-bucket
-aws synthetics update-canary --name theseus-xml-canary --region eu-west-2 --code S3Bucket=theseus-misc-bucket,S3Key=theseus-xml-canary.zip,Handler=theseus-xml-canary.handler --run-config EnvironmentVariables={EXPECTED_RX_LIST="ENCble\,ENCwof\,THES04\,THES05\,THES06\,THES07\,THES12\,THES15"}
+aws s3 cp theseus-xml-canary.zip s3://theseus-misc-bucket-2
+aws synthetics update-canary --name theseus-xml-canary-2 --region eu-west-1 --code S3Bucket=theseus-misc-bucket-2,S3Key=theseus-xml-canary.zip,Handler=theseus-xml-canary.handler --run-config EnvironmentVariables={EXPECTED_RX_LIST="TMS002\,TMS003\,TMS008\,TMS014\,TMS016\,TMS019\,THES05\,THES06\,THES07"}
